29b733bd313f71bc552bb36b78a65d04666e08ab
jcornelison <james_cornelison@g.harvard.edu>
2019-09-06 08:10:48 -0400

    Major changes to DAQ script

--- LabJack_DAQ.py
+++ LabJack_DAQ.py
@@ -13,14 +13,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # import other packages
-# import usbtmc
+import usbtmc
 import time
 import datetime
-import matplotlib.pyplot as plt
 import serial
-import csv
 import u6
-import sys
 
 try:
     from matplotlib import animation
@@ -29,47 +26,45 @@ except ImportError:
 
 import numpy as np
 import os.path as op
-import select
 import argparse
+from threading import Thread
+
+# Global Variables
+thstat = True
+
+###############################################
+def auto_daq(ch,dmmtype):
+    global thstat
+    while thstat:
+        get_data(ch,dmmtype)
+    return
 
 
 ###############################################
-
 # Subfunction for data recording and logging.
+def get_data(ch,dmmtype):
+    b = []
+    # elif avg == False:
+    #  b = np.zeros((1,3))
+    #  avg = True
+    #  print("Starting integration")
+    #  #note_string = "avging"
+    # else:
+    #  avg = False
+    #  b_mean = np.mean(b[1:,1], axis=0)
+    #  b_err = np.sqrt(np.var(b[1:,1], axis=0)/len(b[1:,2]))
+    #  b_mean2 = np.mean(b[1:,2], axis=0)
+    #  b_err2 = np.sqrt(np.var(b[1:,2], axis=0)/len(b[1:,2]))
+    #  print("Samples: {4}\nMean_1: {0}, Err on Mean_1: {1}\nMean_2: {2}, Err on Mean_2: {3}".format(b_mean, b_err, b_mean2, b_err2, len(b[1:,2])))
+    # del note_string
 
-def getData(num, arr, line):
-    # get dmm voltage
-    # volt = float(dmm.ask(":MEAS:VOLT:DC?"))
-    # os.system('cls' if os.name == 'nt' else 'clear')
-    global a, el_time, proc_start, count, ch
-    global volt, time_data, l1, avg, b, d, fieldnames
-    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-        cmd = raw_input()
-        if cmd == 'stop':
-            exit()
-        elif cmd == 'pause':
-            cmd = raw_input('DAQ paused. Press Enter to continue or enter stop to quit.')
-        # elif avg == False:
-        #  b = np.zeros((1,3))
-        #  avg = True
-        #  print("Starting integration")
-        #  #note_string = "avging"
-        # else:
-        #  avg = False
-        #  b_mean = np.mean(b[1:,1], axis=0)
-        #  b_err = np.sqrt(np.var(b[1:,1], axis=0)/len(b[1:,2]))
-        #  b_mean2 = np.mean(b[1:,2], axis=0)
-        #  b_err2 = np.sqrt(np.var(b[1:,2], axis=0)/len(b[1:,2]))
-        #  print("Samples: {4}\nMean_1: {0}, Err on Mean_1: {1}\nMean_2: {2}, Err on Mean_2: {3}".format(b_mean, b_err, b_mean2, b_err2, len(b[1:,2])))
-        # del note_string
-
-    # realtime = datetime.datetime.now() - time_start
+    # If loop runs faster than the sample rate, average until enough time elapses
     volt = np.zeros(len(ch.split(',')))
     samp_el = 0
     count = 0
     tstart = time.time()
     while samp_el < 1 / sr:
-        v = read_volts(len(ch.split(',')))
+        v = read_volts(len(ch.split(',')),dmmtype)
         volt = volt + np.array(v)
         # rt = datetime.datetime.now() - time_start
         samp_el = time.time() - tstart
@@ -79,108 +74,99 @@ def getData(num, arr, line):
     v = v.tolist()
     el_time = time.time()
     time_data = el_time
-    # volt = sum(volt)/len(volt)
-    # volt2 = sum(volt2)/len(volt2)
-    # v = read_volts(len(ch.split(',')))
+
     if avg:
         avging = 1
     else:
         avging = 0
 
-    # get digital protractor angle
-    # ser.flushInput()
-    # time.sleep(0.1)
-    # angle_str = ser.readline()
-    # angle_str = angle_str.split()
-    # inc_angle = FindAngle(angle_str)
+    # get inclinometer angle
+    inc_angle = []
+    if inc:
+        ser.flushInput()
+        time.sleep(0.1)
+        angle_str = ser.readline()
+        angle_str = angle_str.split()
+        inc_angle = findangle(angle_str)
 
     # plot voltage vs Time
     z = [el_time]
-    z2 = "{0}".format(el_time)
     for i in range(0, len(ch.split(','))):
         z.append(v[i])
-        z2 = z2 + ",{0}".format(v[i])
-    # z.append(avging)
-    z2 = z2 + ",{0}".format(avging)
-    # print z2
-    inc = 1
-    # line.clear()
 
     if avg:
         b = np.append(b, z, axis=0)
-        if PLOT == True:
-            a = np.append(a, z[0:len(z) - 1], axis=0)
-            line.plot(a[1:, 0], a[1:, 1], '.g', b[1:, 0], b[1:, 1], '.r')
-            line2.plot(a[1:, 0], a[1:, 2], '.y', b[1:, 0], b[1:, 2], '.b')
-    else:
-        if PLOT == True:
-            a = np.append(a, [z], axis=0)
-            line.plot(a[1:, 0], a[1:, 1], '.g')
-            # line2.plot(a[1:, 0], a[1:, 2], '.y')
-    # save data in text and csv format
-    if note_string:
-        # f.write("%s\t%.6f\t%s\n" %(datetime.datetime.now(),volt,note_string))
-        with open(filenamex, 'a') as csvfile:
-            # fieldnames = ['Time','DMM_volt','volt2','avging_on', 'Notes']
-            # writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            z2 = z2 + "," + note_string
-            csvfile.writelines(z2)
-            csvfile.write("\n")
-    else:
-        # f.write("%s\t%.6f\n" %(datetime.datetime.now(),volt))
-        with open(filenamex, 'a') as csvfile:
-            # fieldnames = ['Time','DMM_volt','volt2','avging_on']
-            # writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            # writer.writerow(z)
-            csvfile.writelines(z2)
-            csvfile.write("\n")
-    return line,
+
+    fstring = write_data(filenamex, el_time, v, inc_angle, avging)
+
+    return fstring
 
 
 ###############################################
-def run_ani():
-    global a, ch
+def write_data(filename, time, volts, angle, avging):
+    fs = "{0}".format(time) # Filestring
 
-    fig1 = plt.figure()
+    # If Inclinometer is on, put that first.
+    if angle:
+        fs = fs + ",{0}".format(angle)
 
-    time = 10
-    a = np.zeros((1, len(ch.split()) + 1))
-    # print a
-    l = fig1.add_subplot(1, 1, 1)
-    l.set_xlabel("Time (s)")
-    l.set_ylabel(ylab)
+    for i in range(0,len(volts)):
+        fs = fs + ",{0}".format(volts[i])
 
-    # l2 = fig1.add_subplot(2,1,2)
-    # l2.set_xlabel("Time (s)")
-    # l2.set_ylabel(ylab)
+    fs = fs + ",{0}".format(avging)
 
-    try:
-        fig1_ani = animation.FuncAnimation(fig1, getData, 100, fargs=(a, l), interval=50, blit=False)
-    except KeyboardInterrupt:
-        pass
-    return plt.show()
-
+    # save data in text and csv format
+    with open(filename, 'a') as csvfile:
+        csvfile.writelines(fs)
+        csvfile.write("\n")
+    return fs
 
 ###############################################
 def get_field_names(ch, inc):
-    S = ch.split(',')
+    s = ch.split(',')
     fld_name = ["Time"]
     if inc:  # If inclinometer is toggled on, put that first.
         fld_name.append(",Angle")
-    for i in range(0, len(S)):  # Add channel names
-        fld_name.append(",{0}".format(S[i]))
+    for i in range(0, len(s)):  # Add channel names
+        fld_name.append(",{0}".format(s[i]))
 
     fld_name.append(",avging_on")
-    # fld_name.append(",Notes")
     return fld_name
 
 
 ###############################################
-def read_volts(ch):
+def read_volts(ch, dmmtype):
     v = []
-    for i in range(0, int(ch)):
-        v.append(adc.getAIN(i))
+    if dmmtype == "u6":
+        for i in range(0, int(ch)):
+            v.append(dmm.getAIN(i))
+    elif dmmtype == "agil":
+        v = float(dmm.ask(":MEAS:VOLT:DC?"))
+    elif dmmtype == "test":
+        for i in range(0, int(ch)):
+            v.append(np.random.randn())
+    else:
+         raise ValueError("Acquisition Type not recognized.")
     return v
+
+
+###############################################
+# Subfunction for extracting angle from inclinometer
+# (convert ascii text to float)
+def findangle(angle_str):
+    angle = 0
+    if angle_str:
+        if angle_str[0] == b'+':
+            angle_sign = 1
+            angle = -1 * angle_sign * float(angle_str[1])
+        if angle_str[0] == b'-':
+            angle_sign = -1
+            angle = -1 * angle_sign * float(angle_str[1])
+        if len(angle_str) == 1:
+            angle = -1 * float(angle_str[0])
+    else:
+        angle = 0
+    return angle
 
 
 ###############################################
@@ -189,9 +175,10 @@ def get_args():
     parser.add_argument("--temp", nargs=2, default=(0, 0), help="Hot/cold load power value (NOT WORKING)", type=float)
     parser.add_argument("--title", help="Change the default filename. def = {0}".format(def_filenamex),
                         default=def_filenamex)
-    parser.add_argument("--plot", help="Toggles plotting feature. (Normally off)", default=False, action="store_true")
-    # parser.add_argument("--rx", help = "RX name in filename", default = def_rx, type = int)
-    # parser.add_argument("--el", help = "el in filename", default = def_el, type = float)
+    parser.add_argument("--dmm", help="Utilize Agilent DMM instead of U6 for DAQ, only 1 channel. (Normally off)",
+                        default=False, action="store_true")
+    parser.add_argument("--test", help="Disables DAQs and writes fake data for testing. (Normally off)",
+                        default=False, action="store_true")
     parser.add_argument("--dir", help="dir in filename", default=def_dir)
     parser.add_argument("--ch",
                         help="Select number of channels up to 13 by naming them. E.g. \"T,V,T2,V2\" (for labjack only)",
@@ -200,7 +187,22 @@ def get_args():
                         action="store_true")
     parser.add_argument("--sr", help="Set sample rate in samps / sec (only when not plotting) Default = 1", default=1.0,
                         type=float)
+    parser.add_argument("--ow", help="Overwrite input file. (Normally off)",
+                        default=False, action="store_true")
+
     return parser, parser.parse_args()
+
+
+###############################################
+def print_menu():
+    print("##########")
+    print("##########")
+    print("input enter key to take data point OR input integer for multiple points")
+    print("input 'auto' to continuously log data")
+    print("input 'stop' to stop taking data and close recorder")
+    print("##########")
+    print("##########")
+    return
 
 
 ###############################################
@@ -208,108 +210,98 @@ def get_args():
 if __name__ == '__main__':
     # initialize params
     avg = False
-    volt = []
-    el_time = []
 
     def_dir = op.join(op.expanduser("~"), "LabJackDAQ", "data")
-
     def_filenamex = "labjack_generic_daq.csv".format(def_dir, datetime.datetime.now())
     parser, args = get_args()
+
+    sr = args.sr
+    inc = args.inc
+
+    # Initialize DAQ
+    # Prioritize test option, then Agilent DMM, and lastly the LabJack.
+    if args.test:
+        dmmtype = "test"
+    else:
+        if args.dmm:
+            dmmtype = "agil"
+            dmm = usbtmc.Instrument(2391, 1543)
+            dmm.ask("*IDN?")
+        else:
+            dmmtype = "u6"
+            dmm = u6.U6()
+            dmm.getCalibrationData()
+
+    # File management:
     if args.title[-4::] == ".csv":
         args.title = args.title[0:-4]
 
     filenamex = op.join(args.dir, args.title + ".csv")
-
-    # Check if filename exists and append a number to the end of it if so
-    if op.isfile(filenamex):
+    # Check if filename exists. Overwrite or append a number to the end of it to make it unique.
+    if op.isfile(filenamex) and not args.ow:
         run_num = 0
-        print "File: {0} already exists!".format(args.title)
+        print("File: {0} already exists!".format(args.title))
         while op.isfile(filenamex):
             run_num = run_num + 1
             filenamex = op.join(args.dir, args.title + "{0}.csv".format(run_num))
-        print "Using file: " + filenamex
+        print("Using file: " + filenamex)
 
-    ch = args.ch
-    sr = args.sr
-    PLOT = args.plot
-    inc = args.inc
-    # Initialize DAQ
-    adc = u6.U6()
-    adc.getCalibrationData()
+    # Open csvfile
+    with open(filenamex, 'w') as csvfile:
+        fieldnames = get_field_names(args.ch, inc)
+        csvfile.writelines(fieldnames)
+        csvfile.write("\n")
+        print("Writing data to file: {0}".format(filenamex))
 
+    # Initialize inclinometer if wanted
     if inc:
         ser = serial.Serial("/dev/ttyUSB0", baudrate=9600);
         ser.close()
         ser.open()
 
         # Cycle power if digital protractor freezes up
-        print('Cycle Digital Protractor Power to Continue:\n')
+        print('Cycle inclinometer power to Continue:\n')
         ser.readline()
         ser.readline()
 
-    if (args.temp[1] > 0):
-        p1, p2, t1, t2 = float(sys.argv[1]), float(sys.argv[2]), 300, 77.6
-        gain = (p1 - p2) / (t1 - t2)
-        ylab = "Temp (K)"
-    else:
-        gain = 1
-        ylab = "Voltage (V)"
-
-    # open csvfile
-    with open(filenamex, 'w') as csvfile:
-        fieldnames = get_field_names(args.ch, inc)
-        # writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        # writer.writeheader()
-        csvfile.writelines(fieldnames)
-        csvfile.write("\n")
-        print "Writing data to file: {0}".format(filenamex)
-    # print input commands
-    print("input enter key to take data point OR input integer for multiple points")
-    print("input 'auto' to continuously log data")
-    print("input 'pause' to pause logging")
-    print("input 'stop' to stop taking data and close recorder")
-
-    # generate empty note_string
-    note_string = []
-
+    runprog = True
     # main function loop
-    while True:
-
+    while runprog:
+        # print input commands
+        print_menu()
         # ask for input, stop if requested
-        input_string = raw_input()
-        time_start = time.time()
+        input_string = input()
+
+        # If they just hit enter, collect one data point.
+        if not input_string:
+            fs = get_data(args.ch, dmmtype)
+            print(fs)
         if input_string == 'stop':
-            # f.close()
-            break
+            runprog = False
 
         # check for multi-measurement request
         try:
-            burst = int(input_string) - 1
+            burst = int(input_string)
             while (burst > 0):
+                fs = get_data(args.ch, dmmtype)
+                print(fs)
                 burst = burst - 1
-                getData()
-                if PLOT == True:
-                    plt.pause(0.001)
-                    time.sleep(0.1)
         except ValueError:
             pass
 
         # check for auto-measurement request
         if input_string == 'auto':
-            print "Starting data aquisition...\n"
-            print "Enter pause to temporarily stop DAQ or enter stop to close program."
-            try:
-                # while(1):
-                # getData()
+            print("Starting data aquisition...\n")
+            print("Enter pause to temporarily stop DAQ or enter stop to close program.")
+            # Initialize DAQ loop
+            daqthread = Thread(target=auto_daq, args=(args.ch, dmmtype,))
+            daqthread.start()
 
-                if PLOT == True:
-                    run_ani()
-                    plt.show()
-                    plt.pause(0.001)
-                    # time.sleep(0.1)
-                else:
-                    while (1):
-                        getData([], [], [])
-            except KeyboardInterrupt:
-                pass
-
+            cmd = input()
+            if cmd == 'stop':
+                thstat = False
+                runprog = False
+            elif cmd == 'pause':
+                thstat = False
+            else:
+                print("Command not recognized. Try again. (DAQ still running)")
--- plt_data.py
+++ plt_data.py
@@ -9,78 +9,87 @@ import datetime
 import os.path as op
 import glob
 
+
 def get_args():
-  parser = argparse.ArgumentParser(description = 'Plots timestream of data with various options')
-  parser.add_argument("--title", help = "Choose a filename. Defaults to newest csv in directory", default = def_filenamex)
-  parser.add_argument("--dir", help = "dir in filename", default = def_dir)
-  parser.add_argument("--ch", help = "Select number of channels up to 13 by naming them. E.g. \"T,V,T2,V2\" (for labjack only)", default= "AIN0")
-  return parser, parser.parse_args()
+    parser = argparse.ArgumentParser(description='Plots timestream of data with various options')
+    parser.add_argument("--title", help="Choose a filename. Defaults to newest csv in directory", default=def_filenamex)
+    parser.add_argument("--dir", help="dir in filename", default=def_dir)
+    parser.add_argument("--ch",
+                        help="Select number of channels up to 13 by naming them. E.g. \"T,V,T2,V2\" (for labjack only)",
+                        default="AIN0")
+    return parser, parser.parse_args()
+
 
 # Main Program
 if __name__ == '__main__':
-  def_dir = op.join(op.expanduser("~"),"LabJackDAQ","data")
-  def_filenamex = ""
-  parser, args = get_args()
+    def_dir = op.join(op.expanduser("~"), "LabJackDAQ", "data")
+    def_filenamex = ""
+    parser, args = get_args()
 
-
-  if args.title=="":
-    lof = glob.glob(op.join(args.dir,"*.csv"))
-    if len(lof)>0:
-      args.title = max(lof, key=op.getmtime)
-      filenamex = '{0}'.format(args.title)
+    if args.title == "":
+        lof = glob.glob(op.join(args.dir, "*.csv"))
+        if len(lof) > 0:
+            args.title = max(lof, key=op.getmtime)
+            filenamex = '{0}'.format(args.title)
+        else:
+            raise NameError("No CSV files found in {0}".format(args.dir))
     else:
-      raise NameError("No CSV files found in {0}".format(args.dir))
-  else:
-    if not args.title[-4::] == ".csv":
-      args.title = args.title+".csv"
-    filenamex = op.join(args.dir, args.title)
+        if not args.title[-4::] == ".csv":
+            args.title = args.title + ".csv"
+        filenamex = op.join(args.dir, args.title)
 
-  # Check if filename exists
-  if not op.isfile(filenamex):
-    raise NameError("File: {0} doesn't exist!".format(args.title))
-  else:
-    print "Loading: {0}".format(filenamex)
-
-  Ntail = 0
-  fig = plt.figure()
-  plt.interactive(False)
-  while True:
-
-    # Data will be format: rows = samples, columns = channels
-    #d = np.genfromtxt('./20180116_RPS_monitor_2.csv',delimiter=',', skip_header=1)
-    d = np.genfromtxt(filenamex,delimiter=',',names=True)
-    fig.clf()
-    dnames = d.dtype.names
-    t = dnames[0]
-    tn = d[t]
-    plen = len(dnames)-2
-    for i in range(1,plen+1):
-      dn = d[dnames[i]]
-      plt.subplot(plen, 1, i)
-
-      if Ntail == 0:
-        plt.plot(tn, dn)
-
-      else:
-        plt.plot(tn[-Ntail:], dn[-Ntail:])
-
-      plt.title(dnames[i])
-      plt.grid()
-
-
-    plt.xlabel('Time (s)')
-    plt.subplots_adjust(hspace=1.2)
-    plt.show(block=False)
-
-    print("hit 'enter' to refresh or integer N for last N points")
-    time.sleep(1)
-    input_string = raw_input()
-
-    if len(input_string) == 0:
-      Ntail = 0
+    # Check if filename exists
+    if not op.isfile(filenamex):
+        raise NameError("File: {0} doesn't exist!".format(args.title))
     else:
-      try:
-        Ntail = int(input_string)
-      except KeyboardInterrupt:
-        exit()
-    print "Loading the last {0} points...".format(Ntail)
+        print("Loading: {0}".format(filenamex))
+
+    Ntail = 0
+    fig = plt.figure()
+    plt.interactive(False)
+
+    runloop = True
+    while runloop:
+
+        # Data will be format: rows = samples, columns = channels
+        # d = np.genfromtxt('./20180116_RPS_monitor_2.csv',delimiter=',', skip_header=1)
+        d = np.genfromtxt(filenamex, delimiter=',', names=True)
+        fig.clf()
+        dnames = d.dtype.names
+        t = dnames[0]
+        tn = d[t]
+        plen = len(dnames) - 2
+        for i in range(1, plen + 1):
+            dn = d[dnames[i]]
+            plt.subplot(plen, 1, i)
+
+            if Ntail == 0:
+                plt.plot(tn, dn)
+
+            else:
+                plt.plot(tn[-Ntail:], dn[-Ntail:])
+
+            plt.title(dnames[i])
+            plt.grid()
+
+        plt.xlabel('Time (s)')
+        plt.subplots_adjust(hspace=1.2)
+        plt.show(block=False)
+
+        print("hit 'enter' to refresh or integer N for last N points")
+        print("Enter 'quit' to exit")
+        time.sleep(1)
+        input_string = input()
+
+        if len(input_string) == 0:
+            Ntail = 0
+        elif input_string=="quit":
+            runloop = False
+            print("Complete!")
+        else:
+            try:
+                Ntail = int(input_string)
+                print("Loading the last {0} points...".format(Ntail))
+            except KeyboardInterrupt:
+                exit()
+
