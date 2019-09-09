f3e0fc8a14fba882a776d891301334fe5122d012
calctrl7 <james_cornelison@g.harvard.edu>
2019-09-09 16:36:43 -0400

    fixed bug in plt_data

--- plt_data.py
+++ plt_data.py
@@ -33,7 +33,7 @@ def do_plots(filename, ntail, figure):
     tn -= tn[0]
     plen = len(dnames) - 2
     fig.clf()
-    axes = fig.subplots(plen, 1, squeeze=False)
+    axes = fig.subplots(plen, 1)
 
     for i in range(1, plen+1):
         dn = data[dnames[i]]
