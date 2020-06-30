d3b5aac31de2615ac8d7586ef5b4213a60778f4a
keck <james_cornelison@g.harvard.edu>
2020-06-29 20:44:23 -0400

    Updated and tested install script

--- Installation/install_ljb.sh
+++ Installation/install_ljb.sh
@@ -7,7 +7,6 @@ go ()
 	if [ $ret -ne 0 ]; then
 		echo "Error, please make sure you are running this script as:"
 		echo "  $ sudo $0"
-		echo "If you are still having problems, please contact LabJack support: <$SUPPORT_EMAIL>"
 		exit $ret
 	fi
 }
@@ -18,25 +17,33 @@ if [ "$OSTYPE" -ne "linux-gnu" ]; then
 fi
 
 # Get python and pip
-go apt-get install python3
-go apt-get install python3-pip
+printf "\n\n\nInstalling Python3/pip...\n\n\n"
+go apt-get -y install python3
+go apt-get -y install python3-pip
 
 # Ensure we have build-ess., libusb, and git for exodriver
-go apt-get install build-essential
-go apt-get install libusb-1.0-0-dev
-go apt-get install git-core
+printf "\n\n\nChecking for required packages...\n\n\n"
+go apt-get -y install build-essential
+go apt-get -y install libusb-1.0-0-dev
+go apt-get -y install git-core
 
 # Get and install exodriver
+printf "\n\n\nInstalling Exodriver...\n\n\n"
 go git clone git://github.com/labjack/exodriver.git
 go cd exodriver/
 go ./install.sh
 go cd ..
 
 # Get labjackpython
+printf "\n\n\nInstalling labjackpython...\n\n\n"
 go git clone git://github.com/labjack/LabJackPython.git
 go cd LabJackPython/
-go python3 setup.py install
+go python3 -m pip install --user .
+go cd ..
 
 # Now setup LabJackDAQ
-go cd ../..
-go python3 setup.py install
+printf "\n\n\nSetting up LabJackDAQ...\n\n\n"
+go git clone git://github.com/jacornelison/LabJackDAQ.git
+go cd LabJackDAQ
+go git checkout plotting_overhaul
+go python3 -m pip install --user .
